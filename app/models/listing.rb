34b9727c669dafb444e0fb16e0b8e19212c2f2b9
Harish Upadhyayula <harish.upad@gmail.com>
2015-09-06 01:47:43 -0500

    Added user to the listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,4 +9,10 @@ class Listing < ActiveRecord::Base
 					  	:secret_access_key => ENV['aws_secret_access_key']
 					  }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	validates :name, :description, :price, presence: true
+	validates :price, numericality: {greater_than: 0}
+	validates_attachment_presence :image
+
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905133033) do
+ActiveRecord::Schema.define(version: 20150906064500) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150905133033) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
