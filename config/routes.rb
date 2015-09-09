6d1faf08be9f83e62ed717274740d3c633089b24
Harish Upadhyayula <harish.upad@gmail.com>
2015-09-08 21:35:34 -0500

    Enable users to place order

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  resources :listings
+  resources :listings do
+  	resources :orders
+  end
   get 'pages/About'
   get 'pages/Contact'
   get 'seller' => 'listings#seller'
